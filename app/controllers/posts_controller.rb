7a52b3108107ceae99069d15512b1746914a5173
murandot <103624383+murandot@users.noreply.github.com>
2022-04-22 20:49:56 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    redirect json:{ post: post }
+    render json:{ post: post }
   end
 end
